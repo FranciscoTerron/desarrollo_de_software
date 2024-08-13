1961674618d478efc27812bf1f899eaa0eb060ba
FranciscoTerron <franciscoterrondg@hotmail.com>
2024-08-13 13:03:04 -0300

    Ejercicio3_subido

--- ejercicios_python/ejercicio3.py
+++ ejercicios_python/ejercicio3.py
@@ -13,20 +13,20 @@ def resta(n,m):
 # Funcion para el menu
 def menu (opcion):
     if(opcion == 1):
-        suma(n1,n2)
+        print("La sumatoria es"+ " " + str(suma(n1,n2)))
     elif(opcion == 2):
-        multiplicacion(n1,n2)
+       print("La resta es" + " " +str(resta(n1,n2)))
     elif(opcion == 3):
-        division(n1,n2)
+        print("La multiplicacion es" + " " +str(multiplicacion(n1,n2)))
     elif(opcion == 4):
-        resta(n1,n2)
+       print("La division es" + " "+  str(division(n1,n2)))
     else:
         print("Valor ingresado no valido")
 
 
-n1 = print("Ingrese el primer valor")
-n2 = print("Ingrese el segundo valor")
-opcion = print("Ingrese 1 para sumar, 2 para restar , 3 para multiplicar, 4 para dividir")
+n1 = int(input("Ingrese el primer valor:\n"))
+n2 = int(input("Ingrese el segundo valor:\n"))
+opcion = int(input("Ingrese 1 para sumar, 2 para restar , 3 para multiplicar, 4 para dividir\n"))
 menu(opcion)
 
 
