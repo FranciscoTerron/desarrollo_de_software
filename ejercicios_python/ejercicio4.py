b27e29d13f9f5801708b1b7e090b0bc4c4cd488d
FranciscoTerron <franciscoterrondg@hotmail.com>
2024-08-13 22:07:34 -0300

    Ejercicio4_terminado

--- ejercicios_python/ejercicio4.py
+++ ejercicios_python/ejercicio4.py
@@ -1,16 +1,20 @@
-def escala_celsius (valorNumerico,escala):
-    return (1.8) * valorNumerico + 32
-    
+def celsius_a_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
 
-def escala_fahrenheit(valorNumerico,escala):    
-    sd    
-    
-    
+def fahrenheit_a_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
    
  
 
 
-numero = float(input("Ingrese temperatura")) 
-escala = str(input("Ingrese escala"))
+temperatura = float(input("Ingrese temperatura : ")) 
+escala = (input("Ingrese escala: "))
 
-escala_celsius(numero,"C")
+if escala == "C":
+    resultado = celsius_a_fahrenheit(temperatura)
+    print(f"Resultado: {resultado:.1f}°F")
+elif escala == "F":
+    resultado = fahrenheit_a_celsius(temperatura)
+    print(f"Resultado:{resultado:.1f}°F")
+else:
+    print("La escala ingresada no es valida")
