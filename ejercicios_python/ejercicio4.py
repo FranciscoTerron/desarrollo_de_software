894707308b2e55c396b5d50bdded622ef054aece
FranciscoTerron <franciscoterrondg@hotmail.com>
2024-08-13 22:00:31 -0300

    Agregue carpetas

---
+++ ejercicios_python/ejercicio4.py
@@ -0,0 +1,16 @@
+def escala_celsius (valorNumerico,escala):
+    return (1.8) * valorNumerico + 32
+    
+
+def escala_fahrenheit(valorNumerico,escala):    
+    sd    
+    
+    
+   
+ 
+
+
+numero = float(input("Ingrese temperatura")) 
+escala = str(input("Ingrese escala"))
+
+escala_celsius(numero,"C")
