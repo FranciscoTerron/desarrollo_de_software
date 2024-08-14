c89e9df473b2ec19b8a80bff4d53f54bfbb462ae
FranciscoTerron <franciscoterrondg@hotmail.com>
2024-08-13 22:24:01 -0300

    Agregue_ejercicio5_faltaResolver

---
+++ ejercicios_python/ejercicio5.py
@@ -0,0 +1,18 @@
+def obtenerDatosPeliculas (peliculas):
+    i = 0;
+    for i in range(5):
+        nombre = input("Ingrese el nombre de la pelicula ")
+        anio = input("Ingrese el año de la pelicula ")
+    
+    peliculas.append(nombre,anio)
+    return peliculas
+
+
+def ordenarPeliculas (peliculas):
+    
+    sda
+    
+   
+    
+    
+    
